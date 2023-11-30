ed308685a1bc931a89435411a95187a5010e4f62
Anwar Hussaini <anwarhussaini160@gmail.com>
2023-11-30 23:13:36 +0430

    Add expense tests

---
+++ spec/models/expense_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  describe 'instances' do
+    let!(:user) do
+      User.create(
+        full_name: 'John Doe',
+        email: 'john@example.com',
+        password: 'password123'
+      )
+    end
+
+    let!(:category) do
+      Category.create(
+        name: 'Groceries',
+        user: user
+      )
+    end
+
+    it 'is valid with valid attributes' do
+      expect(category).to be_valid
+    end
+
+    it 'has a name' do
+      expect(category.name).to eq('Groceries')
+    end
+
+    it 'belongs to a user' do
+      expect(category.user).to eq(user)
+    end
+
+    # Add other instance tests as needed
+  end
+end
